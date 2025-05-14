c80bbdbbaa5ff44c3313766c4930e4eef9635d0c
wlghks5759 <wlghks3939@gmail.com>
2025-05-14 18:56:25 +0900

    3

---
+++ vtol_transition/build/CMakeFiles/vtol_transition.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vtol_transition.dir/vtol_transition.cpp.o"
+  "CMakeFiles/vtol_transition.dir/vtol_transition.cpp.o.d"
+  "vtol_transition"
+  "vtol_transition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vtol_transition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ vtol_transition/build/CMakeFiles/vtol_transition.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vtol_transition.
+# This may be replaced when dependencies are built.
